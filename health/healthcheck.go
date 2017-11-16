0cad3b4285074d9a5d3ae217a2944720ddc67ce2
Panziera, Luca <luca.panziera@ft.com>
2017-11-16 12:48:11 +0000

    :ok_hand: renamed service interface to externalService to reduce ambiguity in the code

--- health/healthcheck.go
+++ health/healthcheck.go
@@ -8,19 +8,19 @@ import (
 	"github.com/Financial-Times/service-status-go/gtg"
 )
 
-type service interface {
+type externalService interface {
 	Endpoint() string
 	GTG() error
 }
 
 type HealthService struct {
 	fthealth.HealthCheck
-	rw               service
-	annotationsAPI   service
-	conceptSearchAPI service
+	rw               externalService
+	annotationsAPI   externalService
+	conceptSearchAPI externalService
 }
 
-func NewHealthService(appSystemCode string, appName string, appDescription string, rw service, annotationsAPI service, conceptSearchAPI service) *HealthService {
+func NewHealthService(appSystemCode string, appName string, appDescription string, rw externalService, annotationsAPI externalService, conceptSearchAPI externalService) *HealthService {
 	hcService := &HealthService{
 		rw:               rw,
 		annotationsAPI:   annotationsAPI,
