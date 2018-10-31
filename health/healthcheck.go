e0e4a3fea1f1b7421febf329d7cc7f2cd8ae6c0a
Pete Clark <peter.clark@ft.com>
2018-10-31 14:44:04 +0000

    :loud_noise: :art: Adding log line and refactoring slightly

--- health/healthcheck.go
+++ health/healthcheck.go
@@ -7,6 +7,7 @@ import (
 
 	fthealth "github.com/Financial-Times/go-fthealth/v1_1"
 	"github.com/Financial-Times/service-status-go/gtg"
+	log "github.com/sirupsen/logrus"
 )
 
 type externalService interface {
@@ -16,16 +17,16 @@ type externalService interface {
 
 type HealthService struct {
 	fthealth.TimedHealthCheck
-	rw               externalService
-	annotationsAPI   externalService
-	conceptSearchAPI externalService
+	rw             externalService
+	annotationsAPI externalService
+	conceptRead    externalService
 }
 
-func NewHealthService(appSystemCode string, appName string, appDescription string, rw externalService, annotationsAPI externalService, conceptSearchAPI externalService) *HealthService {
+func NewHealthService(appSystemCode string, appName string, appDescription string, rw externalService, annotationsAPI externalService, conceptRead externalService) *HealthService {
 	hcService := &HealthService{
-		rw:               rw,
-		annotationsAPI:   annotationsAPI,
-		conceptSearchAPI: conceptSearchAPI,
+		rw:             rw,
+		annotationsAPI: annotationsAPI,
+		conceptRead:    conceptRead,
 	}
 	hcService.SystemCode = appSystemCode
 	hcService.Name = appName
@@ -34,7 +35,7 @@ func NewHealthService(appSystemCode string, appName string, appDescription strin
 	hcService.Checks = []fthealth.Check{
 		hcService.rwCheck(),
 		hcService.annotationsAPICheck(),
-		hcService.conceptSearchAPICheck(),
+		hcService.conceptReadCheck(),
 	}
 	return hcService
 }
@@ -51,15 +52,8 @@ func (service *HealthService) rwCheck() fthealth.Check {
 		PanicGuide:       "https://dewey.ft.com/draft-annotations-api.html",
 		Severity:         1,
 		TechnicalSummary: fmt.Sprintf("Generic RW Aurora is not available at %v", service.rw.Endpoint()),
-		Checker:          service.rwChecker,
+		Checker:          service.externalServiceChecker(service.rw, "Generic RW Aurora is healthy"),
 	}
-}
-
-func (service *HealthService) rwChecker() (string, error) {
-	if err := service.rw.GTG(); err != nil {
-		return "", err
-	}
-	return "Generic RW Aurora is healthy", nil
 }
 
 func (service *HealthService) annotationsAPICheck() fthealth.Check {
@@ -70,34 +64,30 @@ func (service *HealthService) annotationsAPICheck() fthealth.Check {
 		PanicGuide:       "https://dewey.ft.com/draft-annotations-api.html",
 		Severity:         1,
 		TechnicalSummary: fmt.Sprintf("UPP Public Annotations API is not available at %v", service.annotationsAPI.Endpoint()),
-		Checker:          service.annotationsAPIChecker,
+		Checker:          service.externalServiceChecker(service.annotationsAPI, "UPP Public Annotations API is healthy"),
 	}
 }
 
-func (service *HealthService) annotationsAPIChecker() (string, error) {
-	if err := service.annotationsAPI.GTG(); err != nil {
-		return "", err
-	}
-	return "UPP Public Annotations API is healthy", nil
-}
-
-func (service *HealthService) conceptSearchAPICheck() fthealth.Check {
+func (service *HealthService) conceptReadCheck() fthealth.Check {
 	return fthealth.Check{
 		ID:               "check-internal-concordances-api-health",
 		BusinessImpact:   "Impossible to serve annotations with enriched concept data to clients",
 		Name:             "Check UPP Internal Concordances API Health",
 		PanicGuide:       "https://dewey.ft.com/draft-annotations-api.html",
 		Severity:         1,
-		TechnicalSummary: fmt.Sprintf("UPP Internal Concordances API is not available at %v", service.conceptSearchAPI.Endpoint()),
-		Checker:          service.conceptSearchAPIChecker,
+		TechnicalSummary: fmt.Sprintf("UPP Internal Concordances API is not available at %v", service.conceptRead.Endpoint()),
+		Checker:          service.externalServiceChecker(service.conceptRead, "UPP Internal Concordances API is healthy"),
 	}
 }
 
-func (service *HealthService) conceptSearchAPIChecker() (string, error) {
-	if err := service.conceptSearchAPI.GTG(); err != nil {
-		return "", err
+func (service *HealthService) externalServiceChecker(svc externalService, okMsg string) func() (string, error) {
+	return func() (string, error) {
+		if err := svc.GTG(); err != nil {
+			log.WithField("url", svc.Endpoint()).WithError(err).Error("Failed to check external service")
+			return "", err
+		}
+		return okMsg, nil
 	}
-	return "UPP Internal Concordances API is healthy", nil
 }
 
 func (service *HealthService) GTG() gtg.Status {
