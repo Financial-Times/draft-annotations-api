12fb354b5d5a19b16a459252e083ef4e58da289f
Kalin Arsov <kalin.arsov@ft.com>
2019-08-13 15:28:25 +0300

    Table driven tests for the mapper package

--- mapper/mapper_test.go
+++ mapper/mapper_test.go
@@ -7,50 +7,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsClassifiedByMappedCorrectly(t *testing.T) {
-
-	originalBody, err := ioutil.ReadFile("test_resources/annotations_isClassifiedBy_v2.json")
-	if err != nil {
-		panic(err)
+func TestConvertPredicates(t *testing.T) {
+	tests := []struct {
+		name            string
+		fixtureBaseName string
+	}{
+		{"IsClassifiedByMappedCorrectly", "annotations_isClassifiedBy"},
+		{"IsPrimariTestConnectionErrorlyClassifiedByMappedCorrectly", "annotations_isPrimarilyClassifiedBy"},
+		{"IsMajorMentionsMappedCorrectly", "annotations_majorMentions"},
+		{"DefaultPassThrough", "annotations_defaults"},
+		{"ImplicitAnnotationsAreFiltered", "annotations_implicit"},
 	}
-	expectedBody, err := ioutil.ReadFile("test_resources/annotations_isClassifiedBy_PAC.json")
 
-	if err != nil {
-		panic(err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			originalBody, err := ioutil.ReadFile("test_resources/" + test.fixtureBaseName + "_v2.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			expectedBody, err := ioutil.ReadFile("test_resources/" + test.fixtureBaseName + "_PAC.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			actualBody, _ := ConvertPredicates(originalBody)
+			assert.JSONEq(t, string(expectedBody), string(actualBody), "they do not match")
+		})
 	}
-	actualBody, _ := ConvertPredicates(originalBody)
-
-	assert.JSONEq(t, string(expectedBody), string(actualBody), "they do not match")
-}
-
-func TestIsPrimariTestConnectionErrorlyClassifiedByMappedCorrectly(t *testing.T) {
-
-	originalBody, err := ioutil.ReadFile("test_resources/annotations_isPrimarilyClassifiedBy_v2.json")
-	if err != nil {
-		panic(err)
-	}
-	expectedBody, err := ioutil.ReadFile("test_resources/annotations_isPrimarilyClassifiedBy_PAC.json")
-
-	if err != nil {
-		panic(err)
-	}
-	actualBody, _ := ConvertPredicates(originalBody)
-	assert.JSONEq(t, string(expectedBody), string(actualBody), "they do not match")
-}
-
-func TestIsMajorMentionsMappedCorrectly(t *testing.T) {
-
-	originalBody, err := ioutil.ReadFile("test_resources/annotations_majorMentions_v2.json")
-	if err != nil {
-		panic(err)
-	}
-	expectedBody, err := ioutil.ReadFile("test_resources/annotations_majorMentions_PAC.json")
-
-	if err != nil {
-		panic(err)
-	}
-	actualBody, _ := ConvertPredicates(originalBody)
-	assert.JSONEq(t, string(expectedBody), string(actualBody), "they do not match")
 }
 
 func TestDiscardedAndEmpty(t *testing.T) {
@@ -62,35 +45,4 @@ func TestDiscardedAndEmpty(t *testing.T) {
 	actualBody, _ := ConvertPredicates(originalBody)
 
 	assert.True(t, actualBody == nil, "some annotations have not been discarded")
-}
-
-func TestDefaultPassThrough(t *testing.T) {
-
-	originalBody, err := ioutil.ReadFile("test_resources/annotations_defaults_v2.json")
-	if err != nil {
-		panic(err)
-	}
-	expectedBody, err := ioutil.ReadFile("test_resources/annotations_defaults_PAC.json")
-	if err != nil {
-		panic(err)
-	}
-	actualBody, _ := ConvertPredicates(originalBody)
-
-	assert.JSONEq(t, string(expectedBody), string(actualBody), "json not equal")
-}
-
-func TestImplicitAnnotationsAreFiltered(t *testing.T) {
-
-	originalBody, err := ioutil.ReadFile("test_resources/implicit_annotations_v2.json")
-	if err != nil {
-		panic(err)
-	}
-	expectedBody, err := ioutil.ReadFile("test_resources/implicit_annotations_PAC.json")
-
-	if err != nil {
-		panic(err)
-	}
-	actualBody, _ := ConvertPredicates(originalBody)
-
-	assert.JSONEq(t, string(expectedBody), string(actualBody), "they do not match")
 }
